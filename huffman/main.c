c307c3e66964df4b0c1b2fd2b5a361016e8f0d93
Kelvin Zhang <zhangxp1998@gmail.com>
2017-03-26 20:58:36 -0400

    add c client for huffman

--- huffman/main.c
+++ huffman/main.c
@@ -1,16 +1,71 @@
 //
 //  main.c
-//  huffman
+//  Burrows Wheeler Compression
 //
 //  Created by Kelvin Zhang on 3/26/17.
 //  Copyright © 2017 Kelvin Zhang. All rights reserved.
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/tty.h>
+#include "encoder.h"
+#include "decoder.h"
 
-int main(int argc, const char * argv[])
+void encode(const char *infile, const char *outfile)
 {
-    // insert code here...
-    printf("Hello, World!\n");
+//    char *infile  = argv[1];
+//    char *outfile = argv[2];
+    
+    int result;
+    
+    Encoder *encoder = encoder_new(infile, outfile);
+    if (encoder == NULL)
+    {
+        printf("Encoder failed to initialize.");
+        exit(1);
+    }
+    
+    result = encoder_encode(encoder);
+    if (result == -1)
+    {
+        printf("Problem occurred during encoding.");
+        exit(1);
+    }
+    
+    result = encoder_free(encoder);
+    if (result == -1)
+    {
+        printf("Encoder failed to free properly.");
+    }
+}
+
+void decode(const char *infile, const char *outfile)
+{
+    // Get a reference to the input and output files:
+//    char *infile  = argv[1];
+//    char *outfile = argv[2];
+    
+    // Create a new decoder:
+    Decoder *decoder = decoder_new(infile, outfile);
+    if (decoder == NULL)
+    {
+        printf("decoder was null\n");
+        exit(1);
+    }
+    
+    // Decode the file:
+    decoder_decode(decoder);
+    
+    // Free up resources:
+    decoder_free(decoder);
+}
+
+int main(int argc, const char *argv[])
+{
+    if(argv[1][0] == '-')
+        encode(argv[2], argv[3]);
+    else if(argv[1][0] == '+')
+        decode(argv[2], argv[3]);
     return 0;
 }
